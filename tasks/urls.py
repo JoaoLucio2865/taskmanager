edd66b3805bce6824c4b2c18edba38c40b8f0042
JoaoLucio2865 <105684589+JoaoLucio2865@users.noreply.github.com>
2024-08-27 21:51:33 -0300

    Add files via upload

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,16 +2,27 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Rota para a página inicial
+    path('', views.index, name='index'),
     path('tasks/', views.task_list, name='task_list'),
     path('task/create/', views.task_create, name='task_create'),
-    path('task/<int:task_id>/comment/create/', views.comment_create, name='comment_create'),
+    path('task/edit/<int:pk>/', views.task_edit, name='task_edit'),
+    path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),
     path('projects/', views.project_list, name='project_list'),
     path('project/create/', views.project_create, name='project_create'),
+    path('project/edit/<int:pk>/', views.project_edit, name='project_edit'),
+    path('project/delete/<int:pk>/', views.project_delete, name='project_delete'),
+    path('priority/edit/<int:pk>/', views.priority_edit, name='priority_edit'),
+    path('priority/delete/<int:pk>/', views.priority_delete, name='priority_delete'),
     path('priorities/', views.priority_list, name='priority_list'),
     path('priority/create/', views.priority_create, name='priority_create'),
+    path('priority/edit/<int:pk>/', views.priority_edit, name='priority_edit'),
+    path('priority/delete/<int:pk>/', views.priority_delete, name='priority_delete'), 
     path('tags/', views.tag_list, name='tag_list'),
     path('tag/create/', views.tag_create, name='tag_create'),
+    path('tag/edit/<int:pk>/', views.tag_edit, name='tag_edit'),
+    path('tag/delete/<int:pk>/', views.tag_delete, name='tag_delete'),
     path('categories/', views.category_list, name='category_list'),
     path('category/create/', views.category_create, name='category_create'),
+    path('category/edit/<int:pk>/', views.category_edit, name='category_edit'),
+    path('category/delete/<int:pk>/', views.category_delete, name='category_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -20,19 +20,23 @@ def task_create(request):
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form})
 
-def comment_create(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
-        form = CommentForm(request.POST)
+        form = TaskForm(request.POST, instance=task)
         if form.is_valid():
-            comment = form.save(commit=False)
-            comment.task = task
-            comment.user = request.user
-            comment.save()
+            form.save()
             return redirect('task_list')
     else:
-        form = CommentForm()
-    return render(request, 'tasks/comment_form.html', {'form': form, 'task': task})
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
 
 def project_list(request):
     projects = Project.objects.all()
@@ -48,6 +52,24 @@ def project_create(request):
         form = ProjectForm()
     return render(request, 'tasks/project_form.html', {'form': form})
 
+def project_edit(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('project_list')
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'tasks/project_form.html', {'form': form})
+
+def project_delete(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('project_list')
+    return render(request, 'tasks/project_confirm_delete.html', {'project': project})
+
 def priority_list(request):
     priorities = Priority.objects.all()
     return render(request, 'tasks/priority_list.html', {'priorities': priorities})
@@ -61,6 +83,24 @@ def priority_create(request):
     else:
         form = PriorityForm()
     return render(request, 'tasks/priority_form.html', {'form': form})
+
+def priority_edit(request, pk):
+    priority = get_object_or_404(Priority, pk=pk)
+    if request.method == 'POST':
+        form = PriorityForm(request.POST, instance=priority)
+        if form.is_valid():
+            form.save()
+            return redirect('priority_list')
+    else:
+        form = PriorityForm(instance=priority)
+    return render(request, 'tasks/priority_form.html', {'form': form})
+
+def priority_delete(request, pk):
+    priority = get_object_or_404(Priority, pk=pk)
+    if request.method == 'POST':
+        priority.delete()
+        return redirect('priority_list')
+    return render(request, 'tasks/priority_confirm_delete.html', {'priority': priority})
 
 def tag_list(request):
     tags = Tag.objects.all()
@@ -76,6 +116,24 @@ def tag_create(request):
         form = TagForm()
     return render(request, 'tasks/tag_form.html', {'form': form})
 
+def tag_edit(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    if request.method == 'POST':
+        form = TagForm(request.POST, instance=tag)
+        if form.is_valid():
+            form.save()
+            return redirect('tag_list')
+    else:
+        form = TagForm(instance=tag)
+    return render(request, 'tasks/tag_form.html', {'form': form})
+
+def tag_delete(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    if request.method == 'POST':
+        tag.delete()
+        return redirect('tag_list')
+    return render(request, 'tasks/tag_confirm_delete.html', {'tag': tag})
+
 def category_list(request):
     categories = Category.objects.all()
     return render(request, 'tasks/category_list.html', {'categories': categories})
@@ -89,3 +147,21 @@ def category_create(request):
     else:
         form = CategoryForm()
     return render(request, 'tasks/category_form.html', {'form': form})
+
+def category_edit(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'tasks/category_form.html', {'form': form})
+
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'tasks/category_confirm_delete.html', {'category': category})
